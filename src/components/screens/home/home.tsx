759416593e4f27872f0e12121ff42a6d164a5b31
ShivaAntier <shiva.mahajan@antiersolutions.com>
2023-09-05 08:12:17 +0530

    navigated to home sreen

--- src/components/screens/home/home.tsx
+++ src/components/screens/home/home.tsx
@@ -1,10 +1,36 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React from 'react';
+import {Text, View, StyleSheet, useColorScheme} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
-function Home (){
-return(
-    <View><Text>adfadsfadfadsf</Text></View>
-)
+function Home() {
+  const isDarkMode = useColorScheme() === 'dark';
+  const textColor: any = {
+    color: isDarkMode ? Colors.lighter : Colors.chartreuse,
+  };
+  const backgroundStyle: any = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+  return (
+    <View style={[style.container, backgroundStyle]}>
+      <Text style={[textColor]}>adfadsfadfadsf</Text>
+    </View>
+  );
 }
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    //   backgroundColor: '#001123',
+  },
+  whiteText: {
+    color: 'White',
+  },
+  blackText: {
+    color: 'Black',
+  },
+});
 
 export default Home;
--- src/components/screens/splash/splash.tsx
+++ src/components/screens/splash/splash.tsx
@@ -1,41 +1,40 @@
-import React,{useEffect} from "react";
-import { Image, View, StyleSheet,StatusBar,useColorScheme} from "react-native";
-import {
-    Colors,
-  } from 'react-native/Libraries/NewAppScreen';
-
-function Splash (navigation:any){
-    useEffect(()=>{
-        setTimeout(()=>{
-            navigation.navigate();
-        }, 2000);
-        },[]);
-    const isDarkMode = useColorScheme() === 'dark';
-    const backgroundStyle:any = {
-        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-      };
-    return(
-        <View style={[styles.container,backgroundStyle]}>
-            <StatusBar backgroundColor="#001123" />
-            <Image style={styles.logo} source={require('../../../images/Splash.png')}/>
-        </View>
-    )
+import React, {useEffect} from 'react';
+import {Image, View, StyleSheet, StatusBar, useColorScheme} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {useNavigation} from '@react-navigation/native';
+function Splash() {
+  const navigation: any = useNavigation();
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.navigate('Home');
+    }, 2000);
+  }, []);
+  const isDarkMode = useColorScheme() === 'dark';
+  const backgroundStyle: any = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+  return (
+    <View style={[styles.container, backgroundStyle]}>
+      <StatusBar backgroundColor="#001123" />
+      <Image
+        style={styles.logo}
+        source={require('../../../images/Splash.png')}
+      />
+    </View>
+  );
 }
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
     //   backgroundColor: '#001123',
-    },
-    logo: {
-      height: 300,
-      width: 300,
-      resizeMode:'contain'
-    },
-    red: {
-      color: 'red',
-    },
-  });
+  },
+  logo: {
+    height: 300,
+    width: 300,
+    resizeMode: 'contain',
+  },
+});
 
 export default Splash;
